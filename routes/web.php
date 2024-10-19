a1edd29637ddf215e18f7661d689cfa5d04937ef
Thoi42 <150762121+Thoi42@users.noreply.github.com>
2024-10-19 10:26:23 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\PostCategoryController;
-
-use App\Http\Controllers\Admin\PostController;
-use App\Http\Controllers\Admin\UploadCKImageController;
-
-use App\Http\Controllers\Admin\DiscountController;
->
 use App\Http\Controllers\Ajax\AjaxDashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController;
@@ -15,7 +9,6 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
-
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -26,12 +19,13 @@ Route::get('/shop', [ProductController::class, 'index'])->name('shop.index');
 Route::get('/shop/{product_slug}',[ProductController::class,'product_details'])->name('shop.product.details');
 Route::get('/login', [UserController::class, 'login'])->name('user.login');
 Route::get('/register', [UserController::class, 'register'])->name('user.register');
+
 Route::get('/cart', [OrderController::class, 'index'])->name('cart.index');
 
 // AJAX
 Route::post('ajax/dashboard/changeStatus', [AjaxDashboardController::class, 'changeStatus'])->name('ajax.dashboard.changeStatus');
 
-
+Route::prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
 
 
@@ -39,38 +33,12 @@ Route::post('ajax/dashboard/changeStatus', [AjaxDashboardController::class, 'cha
     Route::prefix('post')->group(function () {
         
         Route::get('category', [PostCategoryController::class,'index'])->name('postCatagory.index');
-
-        Route::get('category/deleted', [PostCategoryController::class,'deleted'])->name('postCatagory.deleted');
-        Route::get('category/search/{config}', [PostCategoryController::class,'search'])->name('postCatagory.search');
-
-
         Route::get('category/search', [PostCategoryController::class,'search'])->name('postCatagory.search');
-
         Route::get('category/create', [PostCategoryController::class,'create'])->name('postCatagory.create');
         Route::post('category/store', [PostCategoryController::class,'store'])->name('postCatagory.store');
         Route::get('category/edit/{id}', [PostCategoryController::class,'edit'])->name('postCatagory.edit');
         Route::post('category/update/{id}', [PostCategoryController::class,'update'])->name('postCatagory.update');
         Route::delete('category/destroy/{id}', [PostCategoryController::class,'destroy'])->name('postCatagory.destroy');
-        Route::get('category/restore/{id}', [PostCategoryController::class,'restore'])->name('postCatagory.restore');
-        Route::delete('category/forceDelete/{id}', [PostCategoryController::class,'forceDelete'])->name('postCatagory.forceDelete');
     });
-
-    // POST
-    Route::prefix('post')->group(function () {
-        
-        Route::get('/', [PostController::class,'index'])->name('post.index');
-        Route::get('deleted', [PostController::class,'deleted'])->name('post.deleted');
-        Route::get('search/{config}', [PostController::class,'search'])->name('post.search');
-        Route::get('create', [PostController::class,'create'])->name('post.create');
-        Route::post('store', [PostController::class,'store'])->name('post.store');
-        Route::get('edit/{id}', [PostController::class,'edit'])->name('post.edit');
-        Route::post('update/{id}', [PostController::class,'update'])->name('post.update');
-        Route::delete('destroy/{id}', [PostController::class,'destroy'])->name('post.destroy');
-        Route::get('restore/{id}', [PostController::class,'restore'])->name('post.restore');
-        Route::delete('forceDelete/{id}', [PostController::class,'forceDelete'])->name('post.forceDelete');
-    });
-    Route::post('uploadImage', [UploadCKImageController::class, 'upload'])->name('ckeditor.upload');
-});
-
     
 });
