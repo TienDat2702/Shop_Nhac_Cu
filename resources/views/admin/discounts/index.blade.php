e9b889c2707d1bb727113e6c32c143e1ab496506
tainntps27099@fpt.edu.vn <tainntps27099@fpt.edu.vn>
2024-10-30 17:28:13 +0700

    Update index.blade.php

--- resources/views/admin/discounts/index.blade.php
+++ resources/views/admin/discounts/index.blade.php
@@ -29,6 +29,11 @@
                             <th>Mã</th>
                             <th>Tỷ Lệ Giảm Giá</th>
                             <th>Giá Trị Tối Đa</th>
+<<<<<<< Updated upstream
+=======
+                            <th>Số Lượng Mã Giảm Giá</th>
+                            <th>Giá Tối Thiểu</th>
+>>>>>>> Stashed changes
                             <th>Ngày Bắt Đầu</th>
                             <th>Ngày Kết Thúc</th>
                             <th>Hành Động</th>
@@ -40,6 +45,11 @@
                                 <td>{{ $discount->code }}</td>
                                 <td>{{ $discount->discount_rate }}</td>
                                 <td>{{ $discount->max_value }}</td>
+<<<<<<< Updated upstream
+=======
+                                <td>{{ $discount->use_limit }}</td>
+                                <td>{{ $discount->minimum_order_value }}</td>
+>>>>>>> Stashed changes
                                 <td>{{ $discount->start_date }}</td>
                                 <td>{{ $discount->end_date }}</td>
                                 <td>
