a502bad16e8dd6f18420a38bdea7d632485e8b9a
tainntps27099@fpt.edu.vn <tainntps27099@fpt.edu.vn>
2024-10-30 15:57:12 +0700

    Update sidebarUser.blade.php

--- resources/views/user/layouts/component/sidebarUser.blade.php
+++ resources/views/user/layouts/component/sidebarUser.blade.php
@@ -1,6 +1,6 @@
 <li><a href="{{ route('user.profile') }}" class="menu-link menu-link_us-s {{ Route::currentRouteName() == 'user.profile' ? 'menu-link_active' : '' }}">Dashboard</a></li>
 <li><a href="{{ route('user.orders') }}" class="menu-link menu-link_us-s {{ Route::currentRouteName() == 'user.orders' ? 'menu-link_active' : '' }}">Orders</a></li>
-<li><a href="account-address.html" class="menu-link menu-link_us-s ">Addresses</a></li>
-<li><a href="account-details.html" class="menu-link menu-link_us-s">Account Details</a></li>
-<li><a href="account-wishlist.html" class="menu-link menu-link_us-s">Wishlist</a></li>
-<li><a href="login.html" class="menu-link menu-link_us-s">Logout</a></li>
+<li><a href="account-address.html" class="menu-link menu-link_us-s ">Địa chỉ</a></li>
+<li><a href="account-details.html" class="menu-link menu-link_us-s">Thông tin tài khoản</a></li>
+<li><a href="account-wishlist.html" class="menu-link menu-link_us-s">Yêu thích</a></li>
+<li><a href="login.html" class="menu-link menu-link_us-s">Đăng xuất</a></li>
