9822f3626ec95e81b54756cad1edddf8698a14ed
tainntps27099@fpt.edu.vn <tainntps27099@fpt.edu.vn>
2024-11-07 17:43:03 +0700

    Create sidebarUser.blade.php

---
+++ resources/views/user/layouts/component/sidebarUser.blade.php
@@ -0,0 +1,8 @@
+
+<li><a href="{{ route('user.profile') }}" class="menu-link menu-link_us-s {{ Route::currentRouteName() == 'user.profile' ? 'menu-link_active' : '' }}">Dashboard</a></li>
+<li><a href="{{ route('user.orders') }}" class="menu-link menu-link_us-s {{ Route::currentRouteName() == 'user.orders' ? 'menu-link_active' : '' }}">Orders</a></li>
+<li><a href="account-address.html" class="menu-link menu-link_us-s ">Địa chỉ</a></li>
+<li><a href="account-details.html" class="menu-link menu-link_us-s">Thông tin tài khoản</a></li>
+<li><a href="{{ route('wishlist.index') }}" class="menu-link menu-link_us-s {{ Route::currentRouteName() == 'customers.favourites' ? 'menu-link_active' : '' }}">Yêu thích</a></li>
+<li><a href="login.html" class="menu-link menu-link_us-s">Đăng xuất</a></li>
+
