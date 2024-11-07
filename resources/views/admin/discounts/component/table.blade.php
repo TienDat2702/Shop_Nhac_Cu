a609ad3f7934f5543338607e1c57ba42478689ff
tainntps27099@fpt.edu.vn <tainntps27099@fpt.edu.vn>
2024-11-07 17:42:47 +0700

    Update table.blade.php

--- resources/views/admin/discounts/component/table.blade.php
+++ resources/views/admin/discounts/component/table.blade.php
@@ -9,8 +9,13 @@
         <thead>
             <tr>
                 <th class="text-center">STT</th>
+<<<<<<< Updated upstream
                 <th>Tên Thương Hiệu</th>
                 <th width="100px">Ảnh</th>
+=======
+                <th>Tên Giảm Giá</th>
+                <th width="100px">Giá Trị</th>
+>>>>>>> Stashed changes
                 <th>Mô Tả</th>
                 <th>Trạng Thái</th>
                 <th>Thao Tác</th>
@@ -22,6 +27,7 @@
                     <td class="text-center">{{ $brands->currentPage() * $brands->perPage() - $brands->perPage() + $index + 1 }}</td>
                     <td>
                         <div class="name">
+<<<<<<< Updated upstream
                             <a href="#" class="body-title-2">{{ $item->name }}</a>
                         </div>
                     </td>
@@ -29,6 +35,13 @@
                         <span>
                             <img class="img-fluid" src="{{ asset('uploads/brands/' . $item->image) }}" alt="{{ $item->name }}" onerror="this.onerror=null;this.src='path/to/default/image.png';">
                         </span>
+=======
+                            <a href="#" class="body-title-2">{{ $item->discount_name }}</a>
+                        </div>
+                    </td>
+                    <td>
+                        {{ $item->discount_value }}
+>>>>>>> Stashed changes
                     </td>
                     <td>
                         {{ $item->description }}
@@ -36,7 +49,11 @@
                     <td class="text-center">
                         <label class="toggle">
                             <input id="toggleswitch" class="toggleswitch" name="publish" type="checkbox"
+<<<<<<< Updated upstream
                                 value="{{ $item->publish }}" data-id="{{ $item->id }}" data-model="Brand"
+=======
+                                value="{{ $item->publish }}" data-id="{{ $item->id }}" data-model="Discount"
+>>>>>>> Stashed changes
                                 {{ $item->publish == 2 ? 'checked' : '' }}
                                 {{ $config == 'deleted' ? 'disabled' : '' }}>
                             <span class="roundbutton"></span>
@@ -45,12 +62,20 @@
                     <td>
                         <div class="list-icon-function">
                             @if ($config == 'deleted')
+<<<<<<< Updated upstream
                                 <a href="{{ route('brand.restore', $item->id) }}" title="Khôi phục">
+=======
+                                <a href="{{ route('discount.restore', $item->id) }}" title="Khôi phục">
+>>>>>>> Stashed changes
                                     <div class="item edit">
                                         <i class="fa-solid fa-retweet"></i>
                                     </div>
                                 </a>
+<<<<<<< Updated upstream
                                 <form class="form-delete" action="{{ route('brand.forceDelete', $item->id) }}" method="POST">
+=======
+                                <form class="form-delete" action="{{ route('discount.forceDelete', $item->id) }}" method="POST">
+>>>>>>> Stashed changes
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="btn btn-delete item text-danger delete" title="Xóa" 
@@ -59,12 +84,20 @@
                                     </button>
                                 </form>
                             @else
+<<<<<<< Updated upstream
                                 <a href="{{ route('brand.edit', $item->id) }}" title="Chỉnh sửa">
+=======
+                                <a href="{{ route('discount.edit', $item->id) }}" title="Chỉnh sửa">
+>>>>>>> Stashed changes
                                     <div class="item edit">
                                         <i class="icon-edit-3"></i>
                                     </div>  
                                 </a>
+<<<<<<< Updated upstream
                                 <form class="form-delete" action="{{ route('brand.destroy', $item->id) }}" method="POST">
+=======
+                                <form class="form-delete" action="{{ route('discount.destroy', $item->id) }}" method="POST">
+>>>>>>> Stashed changes
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="btn btn-delete item text-danger delete" title="Xóa"
